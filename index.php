b45a7f5cd274fd7698446d10041b9c3313f97860
Mantas <mantas.aaaa@gmail.com>
2019-01-16 21:32:43 +0200

    how to use library example

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+require ('vendor/autoload.php');
+use Folderis\Kodas;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php Kodas::belekas();?></h1>
+</body>
+</html>
